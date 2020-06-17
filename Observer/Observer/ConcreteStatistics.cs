1fde6344ebb6a13d0d4dba4bd1ea8f2c60f04c96
Veronika Nuzhnaya <nika.nuzhna@gmail.com>
2020-06-17 23:37:53 +0300

    Create a Statistics

---
+++ Observer/Observer/ConcreteStatistics.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Observer
+{
+    class ConcreteStatistics
+        : IStatistics
+    {
+        private List<IUser> users;
+
+        public ConcreteStatistics()
+        {
+            users = new List<IUser>();
+        }
+
+        public void AddUser(IUser obs)
+        {
+            users.Add(obs);
+        }
+        
+        public void RemoveUser(IUser obs)
+        {
+            users.Remove(obs);
+        }
+        
+        public void NotifyUsers()
+        {
+            foreach (IUser observer in users)
+                observer.Update();
+        }
+    }
+}
