58ef4aefb2baf14722af3b82ecc59f7f1146a530
Veronika Nuzhnaya <nika.nuzhna@gmail.com>
2020-06-17 23:37:21 +0300

    Create a Statistics Interface

---
+++ Observer/Observer/Interfaces/IStatistics.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Observer
+{
+    interface IStatistics
+    {
+        void AddUser(IUser obs);
+        void RemoveUser(IUser obs);
+        void NotifyUsers();
+    }
+}
